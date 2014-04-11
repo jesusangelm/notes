9cd30a2042e834aa472d5e3f88608beda0f50e63
Jesus Marin <jesusangelm@gmail.com>
2014-04-11 19:19:41 -0430

    add action NEW and CREATE notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,4 +2,23 @@ class NotesController < ApplicationController
   def index
     @notes = Note.all
   end
+
+  def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new(note_params)
+
+    if @note.save
+      redirect_to root_path, notice: "Nota Guardada!"
+    else
+      render :new
+    end
+  end
+
+  private
+  def note_params
+    params.require(:note).permit(:title, :body)
+  end
 end
