01449c1a597e0266e6ff1f181b25c71731ee27a8
Jesus Marin <jesusangelm@gmail.com>
2014-04-11 18:50:39 -0430

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Notepad::Application.config.secret_key_base = '7fe44c046a77d4e4c27695d69974d4a9143e62ecd4437cfb426a6d4777b1b21bcd349ebc370e9511bd54f983f14e35897e21d8dd965d32268a65558f86b0aea4'
